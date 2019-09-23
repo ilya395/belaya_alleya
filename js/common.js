341ec1c06dbe8bb1d14e68439e66e4ce3c6374c9
ilya395 <ilya395.efimenko@yandex.ru>
2019-09-23 04:18:13 +0300

    v_06

--- js/common.js
+++ js/common.js
@@ -76,48 +76,64 @@ $(window).on('load', function() {
 		var startUpperSlaider = upperSlaider();
 
 		// средний слайдер
-		// function middleSlaider() {
-		// 	var slideIndex = 1;
-		// 	showSlides(slideIndex);
-		// 	var slides = $('.about-complex-slaider__slaide');
-		// 	var controlDotsItems = $('.about-complex-slaider__control-buttom');
+		function middleSlaider() {
 
-		// 	$('.about-complex-slaider__arrow-right').on('click', function nextSlide() {
-		// 		showSlides(slideIndex += 1);
-		// 	});
+			var delay = 4000;
+			var timerId = setInterval(function autoNextSlide() {
+				showSlides(slideIndex += 1);
+			}, delay);
 
-		// 	$('.about-complex-slaider__arrow-left').on('click', function previousSlide() {
-		// 		showSlides(slideIndex -= 1);
-		// 	});
+			var slideIndex = 1;
+			
+			var slides = $('.about-complex-slaider__slaide');
+			var controlDotsItems = $('.about-complex-slaider__control-buttom');
+			var slidesInfo = $('.about-complex-slaider__description-slaide');
 
-		// 	controlDotsItems.on('click', function currentSlide(n) {
-		// 	    var n = $(this).index() + 1;
-		// 	    console.log(n);
-		// 	    showSlides(slideIndex = n);
-		//     });
+			showSlides(slideIndex);
+
+			$('.about-complex-slaider__arrow-right').on('click', function nextSlide() {
+				showSlides(slideIndex += 1);
+				clearTimeout(timerId)
+			});
+
+			$('.about-complex-slaider__arrow-left').on('click', function previousSlide() {
+				showSlides(slideIndex -= 1);
+				clearTimeout(timerId)
+			});
+
+			controlDotsItems.on('click', function currentSlide(n) {
+			    var n = $(this).index() + 1;
+			    console.log(n);
+			    showSlides(slideIndex = n);
+			    clearTimeout(timerId)
+		    });
 
 
-		// 	function showSlides(n) {
-		// 		var i;
-
-		// 		if (n > slides.length) {
-		// 			slideIndex = 1;
-		// 		}
-		// 		if (n < 1) {
-		// 			slideIndex = slides.length;
-		// 		}
-		// 		for (i = 0; i < slides.length; i++) {
-		// 			$(slides[i]).removeClass('active');
-		// 		}
-		// 		for (i = 0; i < controlDotsItems.length; i++) {
-		// 			//dots[i].className = dots[i].className.replace("active", "");
-		// 			$(controlDotsItems[i]).removeClass('active');
-		// 		}
-		// 		$(slides[slideIndex - 1]).addClass('active');
-		// 		$(controlDotsItems[slideIndex - 1]).addClass('active');
-		// 	}   
-		// };
-		// var startMiddleSlaider = middleSlaider();
+			function showSlides(n) {
+				var i;
+				console.log(slides.length);
+				if (n > slides.length) {
+					slideIndex = 1;
+				}
+				if (n < 1) {
+					slideIndex = slides.length;
+				}
+				for (i = 0; i < slides.length; i++) {
+					$(slides[i]).removeClass('active');
+				}
+				for (i = 0; i < controlDotsItems.length; i++) {
+					//dots[i].className = dots[i].className.replace("active", "");
+					$(controlDotsItems[i]).removeClass('active');
+				}
+				for (i = 0; i < slidesInfo.length; i++) {
+					$(slidesInfo[i]).removeClass('action');
+				}
+				$(slides[slideIndex - 1]).addClass('active');
+				$(controlDotsItems[slideIndex - 1]).addClass('active');
+				$(slidesInfo[slideIndex - 1]).addClass('action');
+			};
+		};
+		var startMiddleSlaider = middleSlaider();
 
 		//табы
 		var controlTabs = $('.attention-details__control-tab');
@@ -147,6 +163,71 @@ $(window).on('load', function() {
 				$('.general-plan__slaide.general-plan__first-stage').addClass('active');
 				$('.general-plan__slaide.general-plan__all-houses').removeClass('active');
 			};
+		});
+
+		// ход строительства
+		if($('.construction-progress__selectric')){
+			$('.construction-progress__selectric').on('click', function(){
+				$('.construction-progress__select-list').toggleClass('selectric-items');
+
+				$('.select-bar__item').on('click', function(){
+					var listItemValue =  $(this).html();
+					$('.construction-progress__selectric').html('<span class="selectric__label">' + listItemValue + '</span><span class="selectric-arrow">&#10148;</span>');
+					if($('.construction-progress__select-list').hasClass('selectric-items')){
+						$('.construction-progress__select-list').removeClass('selectric-items');
+					};
+				});
+			});
+		};
+
+		function lowerSlaider(){
+			var slideIndex = 1;
+			showSlides(slideIndex);
+
+			$('.note-control__right').on('click', function nextSlide() {
+				showSlides(slideIndex += 1);
+			});
+
+			$('.note-control__left').on('click', function previousSlide() {
+				showSlides(slideIndex -= 1);
+			});
+
+			function showSlides(n) {
+				var i;
+				var slides = $('.construction-progress__slade'); //document.getElementsByClassName("block-view");
+				//controlDotsItems; //var dots = document.getElementsByClassName("control-dots__item");
+
+				if (n > slides.length) {
+					slideIndex = 1;
+				}
+				if (n < 1) {
+					slideIndex = slides.length;
+				}
+				for (i = 0; i < slides.length; i++) {
+					$(slides[i]).removeClass('active');
+				}
+				$(slides[slideIndex - 1]).addClass('active');
+			};
+		};
+
+		var startLowerSlaider = lowerSlaider();
+
+		// owl carousel
+		$('.owl-carousel').owlCarousel({
+			loop:true,
+			margin:0,
+			nav:true,
+			responsive:{
+				0:{
+				    items:1
+				},
+				600:{
+				    items:2
+				},
+				1000:{
+				    items:2
+				}
+			}
 		});
 
 	});
